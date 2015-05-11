7f52fcb6d3028ea246f8837ce8e216632bc042d9
Paul Middlebrooks <pgmiddlebrooks@hotmail.com>
2015-05-11 10:16:51 -0500

    ccm code update

--- create_datafile_pgm/quick_info.m
+++ create_datafile_pgm/quick_info.m
@@ -1,6 +1,19 @@
-%%
+function quick_info(subjectID, sessionID)
+%
+% Quickly asses information from a sesssion.
+% For now, output is only trial number (assessed as number of times monkey
+% began a trial by fixating)
 
-fileName = '/Volumes/SchallLab/data/Xena/Plexon/xp053n01.plx';
+% Load the data
+if strcmp(sessionID(end-3:end), '.mat')
+   sessionID(end-3 : end) = [];
+end
+
+[fileName, localDataPath, localDataFile] = data_file_path(subjectID, sessionID, 'monkey');
+fileName(end-2 : end) = 'plx';
+
+% fileName = '/Volumes/SchallLab/data/Xena/Plexon/xp053n03.plx';
+
 plx = readPLXFileC(fileName,'events');
 
 % Which event do we want to count?
@@ -14,5 +27,7 @@ strobedEventChannel = 17;
 
 
 nTrial = sum(plx.EventChannels(strobedEventChannel).Values == eFixate);
+
+fprintf('Trials: %d\n', nTrial)
 
 
